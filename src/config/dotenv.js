276e3379ee868d0b60d07e68aae4f94a3bdcd107
KNU-K <knukang334@gmail.com>
2024-06-25 03:19:14 +0900

    [init] create configuration file (need to GIT_ID, GIT_PASSWORD)

---
+++ src/config/dotenv.js
@@ -0,0 +1,9 @@
+//require("dotenv").config({ path: `.env.${process.env.NODE_ENV || "dev"}` });
+require("dotenv").config({ path: `.env.${process.env.NODE_ENV || "local"}` });
+
+module.exports = {
+    GITHUB: {
+        ID: process.env.GITHUB_ID,
+        PASSWORD: process.env.GITHUB_PASSWORD,
+    },
+};
